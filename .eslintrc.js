0e5340c7d0d95e34a69fb3be0762f49fd55f7d77
Marcelo <marcelofscarpim@gmail.com>
2021-01-27 22:19:22 -0300

    Aula2 - Configuração do ESLINT e PRETTIER, aplicando o useState

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-bitwise */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': [0, { ignore: [], customValidators: [], skipUndeclared: true }],
+    'react/jsx-props-no-spreading': [0, {
+      html: 'ignore' | 'enforce',
+      custom: 'ignore' | 'enforce',
+      explicitSpread: 'ignore' | 'enforce',
+      exceptions: [],
+    }],
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,30 @@
-import styled from "styled-components";
-import db from "../db.json";
-import Widget from "../src/components/Widget";
-import Footer from "../src/components/Footer";
-import QuizBackground from "../src/components/QuizBackground";
-import GitHubCorner from "../src/components/GitHubCorner";
-import QuizLogo from "../src/components/QuizLogo";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import QuizBackground from '../src/components/QuizBackground';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
 
-function TitleNew(props) {
-  return <h1>{props.children}</h1>;
-}
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `;
 
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-`;
+// function TitleNew(props) {
+//   return <h1>{props.children}</h1>;
+// }
+
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -34,11 +38,15 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
   // return <Title>My page</Title>;
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Tchelo Quiz da F1</title>
+      </Head>
       <QuizContainer>
-        CSS is a shit
         <QuizLogo />
         <Widget>
           <Widget.Header>
@@ -47,6 +55,23 @@ export default function Home() {
 
           <Widget.Content>
             <p>{db.description}</p>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                router.push(`/quiz?name=${name}`);
+                console.log('submit');
+                // manda pra outra página
+              }}
+            >
+              <input
+                placeholder="Informe seu nome"
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
